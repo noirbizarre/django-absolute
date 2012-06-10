14606da0e316ed273501d41a58ef1424f0cbc307
Axel Haustant <noirbizarre@gmail.com>
2012-06-10 20:32:18 +0200

    Added license and classifiers.

--- setup.py
+++ setup.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
-
 from setuptools import setup
 
 setup(
@@ -9,10 +8,22 @@ setup(
     description=__import__('absolute').__description__,
     long_description=open('README.rst').read(),
     url='https://github.com/noirbizarre/django-absolute',
+    download_url='http://pypi.python.org/pypi/django-absolute',
     author='Axel Haustant',
     author_email='noirbizarre+absolute@gmail.com',
-    maintainer='Axel Haustant',
-    maintainer_email='noirbizarre+absolute@gmail.com',
     packages=['absolute', 'absolute.templatetags'],
     install_requires=['django'],
+    license='LGPL',
+    classifiers=[
+        "Framework :: Django",
+        "Development Status :: 4 - Beta",
+        "Programming Language :: Python",
+        "Environment :: Web Environment",
+        "Operating System :: OS Independent",
+        "Intended Audience :: Developers",
+        "Topic :: System :: Software Distribution",
+        "Programming Language :: Python",
+        "Topic :: Software Development :: Libraries :: Python Modules",
+        'License :: OSI Approved :: GNU Library or Lesser General Public License (LGPL)',
+    ],
 )
